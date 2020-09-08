4463f2e7e8e2904fef1a8e27031f87de9e8e4416
amirmaavia <Maaviyamansoori222@gmail.com>
2020-09-08 17:29:19 +0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -3,82 +3,97 @@ export default {
         "AirPods i11-TWS":{
             name: "AirPods i11-TWS",
             img: "https://www.starcity.pk/wp-content/uploads/2019/08/i11-tws-wireless-earbuds.jpg",
-            price: "Rs. 2,449"
+            price: "Rs. 900 "
+        },
+        "AirPods i9S-TWS":{
+            name: "AirPods i9S-TWS",
+            img: "https://static-01.daraz.pk/p/355e3d6b357a50f8614c17e3832c058d.jpg_340x340q80.jpg",
+            price: "Rs. 950 "
         },
         "Wireless HeadSet":{
             name: "Wireless HeadSet",
             img: "https://lh5.googleusercontent.com/QFQsLX_Qhd0kYUU-lOeEDzHaoNljK1xUN4IIhq8PdgcUf0GIaXQswMME7PA0GbMRMOgdqWuc-aPirbvODRjGOEjo5jxESaIOi0MPCAmjhNw8A_F0qQVelIIClv4L=w260",
-            price: "Rs. 1,499"
+            price: "Rs. 300"
         },
         "EARPHONE":{
             name: "EARPHONE",
             img: "https://lh3.googleusercontent.com/8Vrscxv8qeOfpZCP7B72xf0aHt0KeU0GQ-Hg2EwJXGs7GgbiOYay4df-PR0-K5Kr9Vk6-UXWe5fBkvapjlU25KAtuAndtYf0r_UD5XG3LZo_ttHQ6ZRReBLdF7Da=w260",
-            price: "Rs. 1,649"
+            price: "Rs. "
         },
         "EARPHONE R21": {
             name: "EARPHONE R21",
             img: "https://lh4.googleusercontent.com/3xqzNAK5N4BwwxTc-4cVuuQJ6SjXeu3zktF4SMI3dJ_03WFXSjI4IZydpAvWn_h5xShh2Kr0ZFmY3NHXnAJ-JpYjl7h5tDlTGEqAaciyigkdzP1CcvUH081-Jew1=w260",
-            price: "Rs. 2,200",
+            price: "Rs. ",
         },
         "EARPHONE R15": {
             name: "EARPHONE R15",
             img: "https://lh5.googleusercontent.com/QDlaqJR-ZU-PZGZ57akJP-O5ub3c0GoDkeJ8qHEpUkWSTVFBqkMzOYuA8YwKMUFnuRBgHAASkhuVXyzOulOSqQMHyxppQnwiEVKGRU5gdynf_tLunQv6DU2BK5Lr=w260",
-            price: "Rs. 2,250",
+            price: "Rs. ",
         },
         "EARPHONE R32": {
             name: "EARPHONE R32",
             img: "https://lh4.googleusercontent.com/ivZj5wXFFD4Dn4wFSdbqjkM0Xzm4x1IZRU22cEknNLCYGsC6AAsMFtvt0yr3gXED1ZgTzj1DNIRG8cOxYIRa22er2_5r7kzjWQsL5ZgcLwq77G_OetwUl-9FGwkv=w260",
-            price: "Rs. 599",
+            price: "Rs. ",
         },
         "EARPHONE X-6": {
             name: "EARPHONE X-6",
             img: "https://lh5.googleusercontent.com/zqRkPehJjiwQCDRQ3o9K_8KqnplhY06qOt3CB_tL263yYCRixJkjVb1F4CBIvFiXyDZ0oxy32IGKw8tl-aolFPXLHWG05DJJEU6sfAZjNE9XwJeAUUddT1N2tVXk=w260",
-            price: "Rs. 2,060",
+            price: "Rs.",
         },
         "TRIPOD 3110": {
             name: "TRIPOD 3110",
             img: "https://lh4.googleusercontent.com/GgW4Bpp899lk-Cw2pvTnrXmndWvlHPlpo8O_MUwdN8OXA_-cGvfTc5YKmYAUYO5Tp50HJZpk8oHLMm3YOa3_pZf339qdMEAZwoxSRAZZHVKpgy2u4uvMlyNGM8Ev=w260",
-            price: "Rs. 2,300",
+            price: "Rs. ",
         },
         "WIRELESS HEADPHONES P47": {
             name: "Wireless HEADPHONES P47",
             img: "https://lh6.googleusercontent.com/LY56nZeO7qs7YY5YZWZrsEh1dC4cjA_tiUA0pRhGUnM_ubwv7az34SCqFMiQAhjaAbXkv2OtMshLwsIu846WcfKcJkXVsE93WK7qN2zBpngc52_E4tmch6nAAchB=w260",
-            price: "Rs. 2,500",
+            price: "Rs. ",
         },
         "WIRElESS MIC AND SPEAKER": {
             name: "WIRElESS MIC AND SPEAKER",
             img: "https://lh4.googleusercontent.com/s4iO3TWUnQwSLiHU528eqBj8mxEMkXZ-N9SuFZSs2vqoMuETy2yaG608CwIsfaBJO7J3sgiukZxNSAosgc8FALfKNKrbvFOyrazHh0yswEKoczObEq0zrqeg_g-C=w260",
-            price: "Rs. 3,200",
+            price: "Rs. ",
         },
         "POWER BANK AA04": {
             name: "POWER BANK AA04",
             img: "https://lh5.googleusercontent.com/KqF5NWN6sRtETqK9QYqd0skQDIwdbfFbf75w04AFEECtfN8fdxUH_gLQJh_AkTmgb8DPBn7rLfnfwFrAlbN0WUPsAyQuzVoU4VsuAcKD6yNnv4MdihycRJ48poBy=w260",
-            price: "Rs. 1,500",
+            price: "Rs. ",
         },
         "POWER BANK Y12": {
             name: "POWER BANK Y12",
             img: "https://lh3.googleusercontent.com/BPv8QJcA73f7HdEwNaAlawOTHzu-TxPh9x78ClC9glLW5tqiZN1u0HWeYa2KVxjeBXRZY-3QVN5DhymlsCkWoA6N7KDprW67D74zIblHD6-Xdyr5BRKtEiwyrQWe=w260",
-            price: "Rs. 4,200",
+            price: "Rs. ",
         },
         "POWER BANK 119": {
             name: "POWER BANK 119",
             img: "https://lh5.googleusercontent.com/nyQ6pWBeGqBIP-5RHeAAIRQLIbbFx2gAxHWTFQzykQKBc21YWmg2_6aWtBgOwcm7b4vAZxK07D_GdFeTZb7cGN_jJfNuu1t6ioL6CiUsy5y1CEqPYI80GK-9lCQh=w260",
-            price: "Rs. 4,200",
+            price: "Rs. ",
         },
         "BLUETOOTH SPEAKER ZM-1": {
             name: "BLUETOOTH SPEAKER ZM-1",
             img: "https://lh4.googleusercontent.com/nqZza0pql_7csnFLQGXvlQZiO-6XUyJ2UbwXknbpm3rlf6wRw76oip6DcI_KiQhhVuf_Bcf0xKbWpYIEwP_3RqHCfhKtWi_Da8_s6Nmyl3eEBN_nKGfV9AAR3doE=w260",
-            price: "Rs. 4,200",
+            price: "Rs.",
         },
         "SELFIE RING": {
             name: "SELFIE RING",
             img: "https://lh3.googleusercontent.com/ePlpY_P-GgvmPN33UXtJp9oh1boPJB7YedWw9VvnTdQ5OkkTXfECSnR4mbny8zuJcoQ5ysX1iDAGKaYr4-rSKtiOpo7qEKiTe5zfzfEF9FYpI4Sc3tj32yvjFsBf=w260",
-            price: "Rs. 4,200",
+            price: "Rs.",
         },
         "SUPER BASE WOAFER UNIT": {
             name: "SUPER BASE WOAFER UNITs",
             img: "https://lh4.googleusercontent.com/aO5jNWpmnEKfEQ_dg7n19T5cwx-TX612CeiRHPDHzlKycAQFpy9b8vTWO-CBZpCetxvBxz6UsaTTyI_oMshyif2-wYLrTpTiatU3kD5Dlf8Vh8CYIZuQgCpIvBY7=w260",
-            price: "Rs. 4,200",
+            price: "Rs. ",
         },
+        "BackCovers": {
+            name: "Mobile Back Covers ",
+            img: "https://lh3.googleusercontent.com/CutQNy0LeF88LJHt27BtF074IEym28U_j-2G6cMr8HVxNoTh3aEDLt_u6Wf0sTaugeTPKrE6FDP9aql8fS4rrJYKvX4h5dmolSm6vuT3qjfWYeJWWwsX4cPS0vPDRmhqpFYTfBnZzkOx4dI",
+            price: "Rs. ",
+        },
+        "PowerBank-samsung": {
+            name: "PowerBank-samsung 6000mah ",
+            img: "https://lh4.googleusercontent.com/ZkfZpHoyEAh8mRJi66PhpccBz9DJuCcrEl4P0VYXLWONYji7AjVurskP_J-z6G9etNJ2QkjyV7p5B7nQId2Cmq_pF7SbbbiqfAr2Qu2Ul5FnzowZ1qDxtwEPhSla=w260",
+            price: "Rs.1200",
+        }
     }
 }
